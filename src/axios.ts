35e53e308165a1dfd3fd195636092f88c332843d
unknown <phungminhhoang174@gmail.com>
2023-12-25 17:55:12 +0700

    fix: update refresh token logic

--- src/axios.ts
+++ src/axios.ts
@@ -2,9 +2,11 @@ import { HttpError } from "@refinedev/core";
 import axios, { type AxiosResponse } from "axios";
 import createAuthRefreshInterceptor from "axios-auth-refresh";
 import { REFRESH_TOKEN_KEY, TOKEN_KEY, USER_INFO } from "./constants";
-import { redirect } from "react-router-dom";
 
 const axiosInstance = axios.create({
+  baseURL: "https://internal.birdeye.so",
+});
+const axiosRefreshInstance = axios.create({
   baseURL: "https://internal.birdeye.so",
 });
 
@@ -32,35 +34,35 @@ axiosInstance.interceptors.response.use(
 );
 
 // Function that will be called to refresh authorization
-const refreshAuthLogic = () => {
+const refreshAuthLogic = async () => {
   const token = localStorage.getItem(REFRESH_TOKEN_KEY);
 
-  if (!token) {
-    redirect("/login");
-    return Promise.resolve();
-  }
-
-  return axiosInstance
-    .post("/auth/refreshToken", {
+  try {
+    const response = await axiosRefreshInstance.post("/auth/refreshToken", {
       token,
-    })
-    .then((response) => {
-      const { accessToken, refreshToken } = response.data;
-      localStorage.setItem(TOKEN_KEY, accessToken);
-      localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
-
-      return Promise.resolve();
-    })
-    .catch(() => {
-      localStorage.removeItem(TOKEN_KEY);
-      localStorage.removeItem(REFRESH_TOKEN_KEY);
-      localStorage.removeItem(USER_INFO);
-
-      redirect("/login");
-      return Promise.resolve();
     });
+
+    const { accessToken, refreshToken } = response.data;
+    localStorage.setItem(TOKEN_KEY, accessToken);
+    localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
+
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+const refreshFailLogic = () => {
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(REFRESH_TOKEN_KEY);
+  localStorage.removeItem(USER_INFO);
+
+  window.location.href = "/login";
+  return Promise.resolve();
 };
 
 createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic);
+createAuthRefreshInterceptor(axiosRefreshInstance, refreshFailLogic, {
+  statusCodes: [401, 422, 403],
+});
 
 export default axiosInstance;
